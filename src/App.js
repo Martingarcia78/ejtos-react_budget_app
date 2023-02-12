fc650ff8a1431022f6b5c973809c535bde14b26b
Martin <martingarcia78@hotmail.com>
2023-02-11 21:04:57 -0500

    Up to task 3

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                <div className='row mt-3'>
+                <div className='row mt-4'>
                     <div className='col-sm'>
                         <Budget />
                     </div>
@@ -22,6 +22,11 @@ const App = () => {
                     </div>
                     <div className='col-sm'>
                         <ExpenseTotal />
+                    </div>
+                    <div className='col-sm'>
+                        <div className='alert alert-secondary'>
+                        Martin
+                        </div>
                     </div>
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,10 +2,42 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget, expenses } = useContext(AppContext);
+    const changeBudget = (ba) => {
+        if(ba > 20000) {
+            alert("Budget can't exceed 20000");
+            return;
+        }
+
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
+
+        if(ba < totalExpenses) {
+            alert("Your allocated budget can't be less than current expenses");
+            return;
+        }
+
+        const new_budget = ba
+
+        dispatch({
+            type: "SET_BUDGET",
+            payload:new_budget,
+        });
+    };
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £
+                <input
+                    required="required"
+                    type = "number"
+                    id = "budget"
+                    value ={budget}
+                    style = {{marginLeft:"0.5rem", size:100}}
+                    onChange={(a) => changeBudget(a.target.value)}></input>
+                    
+            </span>
         </div>
     );
 };
